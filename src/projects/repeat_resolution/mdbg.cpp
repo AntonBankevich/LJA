8692f66f27186263c92bc90f666f7ef66b135e00
seryrzu <seryrzu@gmail.com>
2022-03-21 11:54:25 -0700

    Fix uninit-ed vertex index variable in mdbg

--- src/projects/repeat_resolution/mdbg.cpp
+++ src/projects/repeat_resolution/mdbg.cpp
@@ -10,7 +10,7 @@ std::vector<SuccinctEdgeInfo> MultiplexDBG::SparseDBG2SuccinctEdgeInfo(
     dbg::SparseDBG &dbg, const UniqueClassificator &classificator) {
     const std::unordered_map<std::string, uint64_t> vert2ind = [&dbg]() {
       std::unordered_map<std::string, uint64_t> vert2ind;
-      uint64_t cnt;
+      uint64_t cnt{0};
       for (const dbg::Vertex &vertex : dbg.vertices()) {
           const std::string &id = vertex.getId();
           vert2ind.emplace(id, cnt);
