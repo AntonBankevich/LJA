a758f695bc3417c415ecf512010d9fbbdd7c4212
TanyaDvorkina <tanunia@gmail.com>
2022-05-03 18:46:48 +0300

    nanoLJA: create GraphContig

--- src/projects/nano/GraphContig.hpp
+++ src/projects/nano/GraphContig.hpp
@@ -5,13 +5,77 @@
 #ifndef DR_GRAPHCONTIG_H
 #define DR_GRAPHCONTIG_H
 
-class GraphContig{
-public:
-    GraphContig() {
+namespace nano {
 
-    }
-private:
+    struct GraphContig{
+        std::string query;
+        int qLen;
+        int qStart;
+        int qEnd;
+        std::string strand;
+        std::vector<std::string> path;
+        int pathLen;
+        int gStart;
+        int gEnd;
+        int nMatches;
+        std::string cigar;
+        Contig read_str;
 
-};
+        GraphContig(const std::vector<string> &params, const Contig &read_str_) {
+            query = params[0];
+            qLen = std::atoi(params[1].c_str());
+            qStart = std::atoi(params[2].c_str());
+            qEnd = std::atoi(params[3].c_str());
+            strand = params[4];
+            ConstructPath(params[5]);
+            pathLen = std::atoi(params[6].c_str());
+            gStart = std::atoi(params[7].c_str());
+            gEnd = std::atoi(params[8].c_str());
+            nMatches = std::atoi(params[9].c_str());
+            cigar = params[params.size() - 1];
+            read_str = read_str_;
+        }
+
+       void PrintContig() const {
+            std::cerr << "Contig: " << query << " " << qLen << " " << qStart << "-" << qEnd
+                      << " " << strand << " " << Path2Str(path) << " " << pathLen << " "
+                      << gStart << "-" << gEnd << " " << nMatches << std::endl;
+        }
+
+    private:
+
+        std::string Path2Str(const std::vector<std::string>& path) const {
+            std::string s = "";
+            for (auto edge: path) {
+                s += edge + ",";
+            }
+            return s;
+        }
+
+        void ConstructPath(const std::string &path_) {
+            std::string curEdgeId = "";
+            char sign;
+            for (char c: path_) {
+                if (c == '>' or c == '<') {
+                    if (curEdgeId.size() > 0) {
+                        path.push_back(curEdgeId + sign);
+                        curEdgeId = "";
+                    }
+                    if (c == '>') {
+                        sign = '+';
+                    } else {
+                        sign = '-';
+                    }
+                } else {
+                    curEdgeId += c;
+                }
+            }
+            if (curEdgeId.size() > 0) {
+                path.push_back(curEdgeId + sign);
+            }
+        }
+    };
+
+}
 
 #endif //DR_GRAPHCONTIG_H
