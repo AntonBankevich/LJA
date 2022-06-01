38a37f89a39e690925230d42b5c1eb1c31707e99
TanyaDvorkina <tanunia@gmail.com>
2022-06-01 18:29:34 +0300

    nanoLJA: fix alignment loading procedure

--- src/projects/nano/SGraphBuilder.cpp
+++ src/projects/nano/SGraphBuilder.cpp
@@ -2,6 +2,9 @@
 // Created by Tatiana Dvorkina on 12.04.2022.
 //
 
+#include <omp.h>
+#include <common/omp_utils.hpp>
+
 #include "edlib/edlib.h"
 #include "SGraphBuilder.h"
 
@@ -37,6 +40,12 @@ void SGraphBuilder::LoadAlignments(const std::unordered_map<std::string, nano::G
                                    const size_t threads) {
     std::vector<std::string> names;
     for (auto const &[key, val]: alignments) {
+        names.push_back(key);
+    }
+    #pragma omp parallel for num_threads(threads)
+    for (auto const name: names) {
+        const std::string key = name;
+        const nano::GraphContig &val = alignments.at(key);
         int prev_index = -1;
         for (int i = 0; i < val.path.size(); ++ i) {
             const std::string &edge_id = val.path[i];
@@ -44,14 +53,17 @@ void SGraphBuilder::LoadAlignments(const std::unordered_map<std::string, nano::G
                 if (prev_index != -1) {
                     std::string e1 = val.path[prev_index];
                     std::string e2 = edge_id;
-                    std::cerr << "Check " << e1 << " " << e2 << std::endl;
+                    std::vector<std::string> subpath = {val.path.begin() + prev_index, val.path.begin() + i + 1};
+                    //std::cerr << "Check " << e1 << " " << e2 << " " << key << std::endl;
+                    val.PrintContig();
                     if (by_vertex_) {
-                        std::vector<std::string> subpath = {val.path.begin() + prev_index, val.path.begin() + i + 1};
                         std::tie(e1, e2) = ResolveByVertex(subpath, val.read_str);
-                        std::cerr << "Add " << e1 << " " << e2 << std::endl;
+                        //std::cerr << " Add " << e1 << " " << e2 <<  " " << key << std::endl;
+                        //std::cerr << std::endl;
                     }
+#pragma omp critical(add_edge)
                     if (e1 != "" && e2 != "") {
-                        AddEdge(e1, e2);
+                        AddEdge(e1, e2, subpath);
                     }
                 }
                 prev_index = i;
@@ -99,24 +111,38 @@ const multigraph::Edge *GetEdgebyStr(const std::string &edge_id_str, const multi
     return &mg_.edges.at(edge_id);
 }
 
-void SGraphBuilder::AddEdge(const std::string &prev_edge_id, const std::string &edge_id) {
-    if (sgraph_.count(prev_edge_id) == 0) {
-        sgraph_[prev_edge_id] = std::unordered_map<std::string, int>();
+void SGraphBuilder::AddEdge(const std::string &prev_edge_id_str, const std::string &edge_id_str, const std::vector<std::string> &subpath) {
+    const multigraph::Edge *prev_edge_id = GetEdgebyStr(prev_edge_id_str, mg_);
+    const multigraph::Edge *edge_id = GetEdgebyStr(edge_id_str, mg_);
+    std::vector<int> subpath_edge_id;
+    for (int i = 1; i < subpath.size() - 1;  ++ i) {
+        const std::string &edge_id_str = subpath[i];
+        subpath_edge_id.push_back(GetEdgebyStr(edge_id_str, mg_)->getId());
     }
-    if (sgraph_[prev_edge_id].count(edge_id) == 0) {
-        sgraph_[prev_edge_id][edge_id] = 0;
+    if (sgraph_.count(prev_edge_id->getId()) == 0) {
+        sgraph_[prev_edge_id->getId()] = std::unordered_map<int, std::pair<int, std::vector<int>>>();
     }
-    ++ sgraph_[prev_edge_id][edge_id];
+    if (sgraph_[prev_edge_id->getId()].count(edge_id->getId()) == 0) {
+        sgraph_[prev_edge_id->getId()][edge_id->getId()].first = 0;
+    }
+    ++ sgraph_[prev_edge_id->getId()][edge_id->getId()].first;
+    sgraph_[prev_edge_id->getId()][edge_id->getId()].second = subpath_edge_id;
 
-    std::string prev_edge_id_rc = RC(prev_edge_id);
-    std::string edge_id_rc = RC(edge_id);
-    if (sgraph_.count(edge_id_rc) == 0) {
-        sgraph_[edge_id_rc] = std::unordered_map<std::string, int>();
+    const multigraph::Edge *prev_edge_id_rc = prev_edge_id->rc;
+    const multigraph::Edge *edge_id_rc = edge_id->rc;
+    std::vector<int> subpath_edge_id_rc;
+    for (auto it = subpath_edge_id.rbegin(); it != subpath_edge_id.rend(); ++it) {
+        subpath_edge_id_rc.push_back(mg_.edges.at(*it).rc->getId());
     }
-    if (sgraph_[edge_id_rc].count(prev_edge_id_rc) == 0) {
-        sgraph_[edge_id_rc][prev_edge_id_rc] = 0;
+
+    if (sgraph_.count(edge_id_rc->getId()) == 0) {
+        sgraph_[edge_id_rc->getId()] = std::unordered_map<int, std::pair<int, std::vector<int>>>();
     }
-    ++ sgraph_[edge_id_rc][prev_edge_id_rc];
+    if (sgraph_[edge_id_rc->getId()].count(prev_edge_id_rc->getId()) == 0) {
+        sgraph_[edge_id_rc->getId()][prev_edge_id_rc->getId()].first = 0;
+    }
+    ++ sgraph_[edge_id_rc->getId()][prev_edge_id_rc->getId()].first;
+    sgraph_[edge_id_rc->getId()][prev_edge_id_rc->getId()].second = subpath_edge_id_rc;
 }
 
 const multigraph::Edge* SGraphBuilder::GetEdgeByNuc(const char nuc, const multigraph::Edge *edge, bool is_inEdge) {
@@ -148,12 +174,6 @@ const multigraph::Edge* SGraphBuilder::GetEdgeByNuc(const char nuc, const multig
     return nullptr;
 }
 
-const multigraph::Edge *GetEdgebyStr(const std::string &edge_id_str, const multigraph::MultiGraph &mg_) {
-    int edge_id = atoi(edge_id_str.substr(0, edge_id_str.size() - 1).c_str());
-    edge_id = edge_id_str[edge_id_str.size() - 1] == '-'? -edge_id: edge_id;
-    return &mg_.edges.at(edge_id);
-}
-
 std::pair<std::string, std::string> SGraphBuilder::ResolveByVertex(const std::vector<std::string> &subpath, const Contig &read_str) {
     std::string path_str = "";
     if (subpath.size() == 2) {
@@ -163,19 +183,15 @@ std::pair<std::string, std::string> SGraphBuilder::ResolveByVertex(const std::ve
         for (int i = 1; i < subpath.size() - 1; ++ i) {
             const std::string &cur_edge = subpath[i];
             const multigraph::Edge *cur_graph_edge = GetEdgebyStr(cur_edge, mg_);
-            if (cur_edge[cur_edge.size() - 1] == '-') {
-                cur_graph_edge = cur_graph_edge->rc;
-            }
             int v_len = cur_graph_edge->end->size();
             path_str += cur_graph_edge->getSeq().str().substr(0, cur_graph_edge->size() - v_len);
+            //std::cerr << path_str.size() << " " << cur_graph_edge->getId() << " " << v_len << std::endl;
         }
         const std::string &cur_edge = subpath[subpath.size() - 1];
         const multigraph::Edge *cur_graph_edge = GetEdgebyStr(cur_edge, mg_);
-        if (cur_edge[cur_edge.size() - 1] == '-') {
-            cur_graph_edge = cur_graph_edge->rc;
-        }
         int v_len = cur_graph_edge->start->size();
         path_str += cur_graph_edge->getSeq().str().substr(0, v_len);
+        //std::cerr << path_str.size() << " " << cur_graph_edge->getId() << " " << v_len << std::endl;
     }
     EdlibAlignResult result = edlibAlign(path_str.c_str(), path_str.size(),
                                          read_str.str().c_str(), read_str.str().size(),
@@ -187,10 +203,10 @@ std::pair<std::string, std::string> SGraphBuilder::ResolveByVertex(const std::ve
         int end_pos = result.endLocations[0];
         char prev_nuc = read_str.str()[start_pos - 1];
         char post_nuc = read_str.str()[end_pos + 1];
-        std::cerr << path_str.size() << " " << read_str.str().size() << " "
-                 << start_pos << " " << end_pos << " "
-                 << result.editDistance << " nuc1:" << prev_nuc
-                 << " nuc2:" << post_nuc << " " << result.numLocations << std::endl;
+//        std::cerr << path_str.size() << " " << read_str.str().size() << " "
+//                 << start_pos << " " << end_pos << " "
+//                 << result.editDistance << " nuc1:" << prev_nuc
+//                 << " nuc2:" << post_nuc << " " << result.numLocations << std::endl;
         const multigraph::Edge *path_in_edge = GetEdgebyStr(subpath[0], mg_);
         const multigraph::Edge *in_edge = GetEdgeByNuc(prev_nuc, path_in_edge, true);
         const multigraph::Edge *path_out_edge = GetEdgebyStr(subpath[subpath.size() - 1], mg_);
