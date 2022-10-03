3f802d678ecf09a04b0a30dd39f71011871d7b04
Anton Bankevich <anton.bankevich@gmail.com>
2022-10-03 11:55:56 -0700

    Extended functionality of subsequence extraction

--- src/projects/scripts/extract_subsequence.cpp
+++ src/projects/scripts/extract_subsequence.cpp
@@ -2,18 +2,36 @@
 #include <sequences/seqio.hpp>
 
 int main(int argc, char **argv) {
-    CLParser parser({"file=", "contig=", "from=", "to="}, {}, {}, "");
+    CLParser parser({"file=", "radius=50000"}, {"contig", "from", "to", "point"}, {}, "");
     parser.parseCL(argc, argv);
     parser.check();
+    StringContig::homopolymer_compressing = false;
     io::SeqReader reader(parser.getValue("file"));
-    std::string name = parser.getValue("contig");
-    size_t from = std::stoull(parser.getValue("from"));
-    size_t to = std::stoull(parser.getValue("to"));
+    std::vector<std::string> names = parser.getListValue("contig");
+    std::vector<std::string> from_str = parser.getListValue("from");
+    std::vector<std::string> to_str = parser.getListValue("to");
+    std::vector<size_t> from;
+    std::vector<size_t> to;
+    for(size_t i = 0; i < from_str.size(); i++) {
+        from.emplace_back(std::stoull(from_str[i]));
+        to.emplace_back(std::stoull(to_str[i]));
+    }
+    size_t radius = std::stoull(parser.getValue("radius"));
+    for(std::string s : parser.getListValue("point")) {
+        size_t p = std::stoull(s);
+        from.emplace_back(p - std::min(radius, p));
+        to.emplace_back(p + radius);
+    }
+    std::unordered_map<std::string, Contig> contigs;
     for(StringContig contig : reader) {
-        if(contig.id == name) {
-            std::cout << contig.seq.substr(from, to - from) << std::endl;
-            break;
-        }
+        contigs[contig.id] = contig.makeContig();
+    }
+    for(size_t i = 0; i < names.size(); i++) {
+        Contig &contig = contigs[names[i]];
+        std::cerr << names[i] << " " << from[i] << " " << to[i] << " " << contig.size() << std::endl;
+        to[i] = std::min(contig.size(), to[i]);
+        VERIFY(from[i] < to[i]);
+        std::cout << ">" << names[i] + "_" + itos(from[i]) + "_" + itos(to[i]) << "\n" << contig.seq.Subseq(from[i], to[i]) << "\n";
     }
     return 0;
 }
