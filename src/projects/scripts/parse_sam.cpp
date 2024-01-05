922d3ea829aa95a68efe8fdea1d9b57c94caf03f
Anton Bankevich <anton.bankevich@gmail.com>
2024-01-04 22:40:24 -0500

    Fixes

--- src/projects/scripts/parse_sam.cpp
+++ src/projects/scripts/parse_sam.cpp
@@ -16,29 +16,34 @@ std::unordered_map<std::string, Contig> ReadCollection(const std::experimental::
 }
 
 struct LocalAlignment {
-    LocalAlignment(const Segment<Contig> &segFrom, const Segment<Contig> &segTo, AlignmentForm al) : seg_from(
-            segFrom), seg_to(segTo), al(std::move(al)) {}
+    LocalAlignment(const Segment<Contig> &segFrom, const Segment<Contig> &segTo, bool rc, AlignmentForm al) : seg_from(
+            segFrom), seg_to(segTo), rc(rc), al(std::move(al)) {}
 
     Segment<Contig> seg_from;
     Segment<Contig> seg_to;
+    bool rc;
     AlignmentForm al;
 
     double percentIdentity() const {
         size_t m = 0;
         size_t diff = 0;
+        Sequence from = seg_from.fullSeq();
+        Sequence to = seg_to.fullSeq();
+        if(rc)
+            from = !from;
         for(auto c : al.columns()) {
-            if(c.event == CigarEvent::M && seg_from.contig().getSeq()[seg_from.left + c.qpos] == seg_to.contig().getSeq()[seg_to.left + c.tpos])
+            if(c.event == CigarEvent::M && from[c.qpos] == to[c.tpos])
                 m++;
             else
                 diff++;
         }
-        return double(m * 100000 / (m + diff)) * 0.0001;
+        return double(m * 100000 / (m + diff)) * 0.001;
     }
 
     std::string str() const {
         std::stringstream ss;
-        ss << seg_from.size() << " " << seg_from << "->" << seg_to << ":" << percentIdentity() << "\n";
-        ss << al.toShortRec(seg_from.fullSeq(), seg_from.fullSeq());
+        ss << seg_from.size() << " " << seg_from << "->" << seg_to << ":" << rc << ":" << percentIdentity() << "\n";
+        ss << al.toShortRec(seg_from.fullSeq(), seg_to.fullSeq());
         return ss.str();
     }
 };
@@ -64,13 +69,14 @@ int main(int argc, char **argv) {
             continue;
         Contig &query = queries[parsed[0]];
         Contig &ref = refs[parsed[2]];
-        size_t tstart = std::stoull(parsed[3]);
+        size_t tstart = std::stoull(parsed[3]) - 1;
         std::pair<size_t, size_t> qbounds = LRskip(cigar_string);
         size_t qstart = qbounds.first;
-        size_t qend = qbounds.second;
+        size_t qend = query.fullSize() - qbounds.second;
+        bool rc = std::stoull(parsed[1]) &(1ull<<4);
         AlignmentForm al(StringToCigar(cigar_string));
         size_t tend = tstart + al.targetLength();
-        LocalAlignment res({query, qstart, qend}, {ref, tstart, tend}, al);
+        LocalAlignment res({query, qstart, qend}, {ref, tstart, tend}, rc, al);
         if(res.seg_from.size() > 10000 || res.seg_from.size() > res.seg_from.contig().fullSize() * 8 / 10)
             std::cout << res.str() << std::endl;
     }
